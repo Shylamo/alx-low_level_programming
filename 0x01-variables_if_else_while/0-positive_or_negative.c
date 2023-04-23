22f7a206588bcb76c748c67e18bfcfa491fe6f4b
Shylamo <chaima.moutachaouiq@gmail.com>
2023-04-23 16:20:03 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main -Entry point
- *Choosing arandom number and evaluating its value
+ * main - Entry point
+ *Description : 'Choosing arandom number and evaluating its value'
+ *Return: always 0
  */
 int main(void)
 {	int n;
