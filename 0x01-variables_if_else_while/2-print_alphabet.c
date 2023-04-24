44dba82fcb5e08de89cf4cfa6ae036e40154c636
Shylamo <chaima.moutachaouiq@gmail.com>
2023-04-24 15:58:38 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Printing the alphabet in lower cases'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0 ; alpha[i] != '\0' ; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
