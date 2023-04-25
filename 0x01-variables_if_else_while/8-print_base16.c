c22a64b62521eb6a46d318c220390b152c7d5132
Shylamo <chaima.moutachaouiq@gmail.com>
2023-04-24 18:35:11 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'Hexadecimal'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i ;
+
+	for ( i = 48; i <= 57; i++)
+		putchar(i);
+	for ( i = 65; i <= 70; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
