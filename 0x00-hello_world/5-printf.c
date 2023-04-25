5be73e82693fa32cfeab64f96af2f9f427ab4ba2
Shylamo <chaima.moutachaouiq@gmail.com>
2023-04-24 19:16:03 -0700

    sentence

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'Printinga random sentence'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
