f0113ed5bbcdd46729f969059d6b7fae474adbe7
Shylamo <chaima.moutachaouiq@gmail.com>
2023-04-26 16:12:01 -0700

    header file 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-#include <time.h>
 /*
  * Main : - Entry
  * _putchar :  prints putchar sentence
@@ -8,12 +7,7 @@
  * return 0;
  */
 
-int _putchar(void)
-{
-	char x[] = "_putchar\n";
-	int i;
-
-	for (i = 0; x [i] != '\0'; i++)
-		putchar(x[i]);
-	return (0);
+int main(void) {
+    // your program code here
+    return 0;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,13 @@
 
 	int _putchar();
 
+int _putchar(void)
+{
+	char x[] = "_putchar\n";
+	int i;
+
+	for (i = 0; x [i] != '\0'; i++)
+		putchar(x[i]);
+	return (0);
+}
 #endif
