a4f042ae446fb7c30cf1bc6c5d5a7ee4146aad78
Shylamo <chaima.moutachaouiq@gmail.com>
2023-05-04 11:01:44 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ * s1 - parameter that describes the first sttring hello
+ * s2 - parametre that describes the second word world
+ * _strcat - is the function to call
+ * Return: Always 0.
+ */
+
+char *_strcat(char *s1, char *s2)
+{
+	int len1, len2;
+	char *concat;
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	char *concat = malloc(sizeof(len1 + len2 + 1));
+
+	strcpy(concat, s1);
+	strcat(concat, s2);
+
+	return (concat);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char* _strcat(char*s1, char*s2);
+
+#endif /* MAIN_H */
